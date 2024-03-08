dd0a0eb71cbc142ba5a51c3c984d1c800f43cf2b
saileshray2023 <saileshray295@gmail.com>
2024-03-08 11:19:47 +0530

    Created a new file NumberOfDivisors.java for finding the number of divisors of the number ,that is divided by 2

---
+++ Assignment_1/src/questions_of_1/NumberOfDivisors.java
@@ -0,0 +1,23 @@
+package questions_of_1;
+
+import java.util.Scanner;
+//hubbi
+public class NumberOfDivisors {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        int c=0;
+        if(n<2)
+            System.out.println("Invalid Number");
+        else{
+            while(n>=2){
+                n/=2;
+                c++;
+
+            }
+        }
+        System.out.println("Number of Divisors are:"+c);
+    }
+
+}
