243557d7cd6d8c6aa93bc9a8202e31ed463cf92f
saileshray2023 <saileshray295@gmail.com>
2024-03-14 01:54:35 +0530

    Changed the for loop contents with required logic

--- Assignment_1/src/questions_of_1/MaxAndMin.java
+++ Assignment_1/src/questions_of_1/MaxAndMin.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class MaxAndMin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int ar[],max=Integer.MIN_VALUE,min=Integer.MAX_VALUE,c1=0,c2=0,pos1=-1,pos2=-1;
+        int[] ar;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int c1=0;
+        int c2=0;
+        int pos1=-1;
+        int pos2=-1;
         System.out.println("Enter the array limit");
         int n = sc.nextInt();
         if (n > 0) {
@@ -12,22 +18,23 @@ public class MaxAndMin {
             for (int i = 0; i < n; i++) {
                 ar[i] = sc.nextInt();
                 if (ar[i]>max){
+
                     max=ar[i];
                     c1=1;
-                    pos1=-1;
-                    if(pos1==-1)
-                        pos1=i;
+                    pos1=i;
 
                 }
-                else if(ar[i]<min){
+                else if (ar[i]==max)
+                    c1++;
+                 if(ar[i]<min){
+
                     min=ar[i];
                     c2=1;
                     pos2=i;
 
-                } else if (ar[i]==max)
-                    c1++;
+                }
 
-                else if(ar[i]==min){ c2++;pos2=i;}
+               else if(ar[i]==min){ c2++;pos2=i;}
 
             }
             System.out.printf("Maximum:%d Occurrences:%d 1stPosition:%d",max,c1,pos1);
