48e7e193c060cbe4663782830513427a6e2b7702
saileshray2023 <saileshray295@gmail.com>
2024-04-25 23:48:30 +0530

    Code to learn raising some exceptions

---
+++ Assignment3/src/ExceptionFinding.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionFinding {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter how many  colors:");
+        try{
+            int n= sc.nextInt();
+            String []m=new String[n];
+            for(int i=0;i<n;i++)
+                m[i]=sc.next();
+            System.out.println("Following are your favourite colors:");
+            for(int i=0;i<n;i++)
+                System.out.println(m[i]);
+        }catch (InputMismatchException e){
+            System.out.println("Please enter appropriate data input!! ");
+
+        }
+        catch (NegativeArraySizeException n){
+            System.out.println("Array index can not be negative!!");
+        }
+
+    }
+
+}
