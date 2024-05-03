dc1cd98a1608a348bb38ddcb2ab698956644c209
saileshray2023 <saileshray295@gmail.com>
2024-05-03 23:17:43 +0530

    Question 4 of Assignment 3

---
+++ Assignment3/src/Q4.java
@@ -0,0 +1,59 @@
+class Box<T>{
+    T var1;
+    Box(T i){
+        var1=i;
+    }
+    void setInstance(T i) {
+        var1=i;
+    }
+       T getInstance() {
+        return var1;
+    }
+    void display() {
+        System.out.println(var1);
+    }
+}
+public class Q4 {
+    public static void main(String[] args) {
+        Box<String> box1,box2;
+        box1= new Box<>("Hello");
+        box2=box1;
+        System.out.println("box1:");
+        box1.display();
+        System.out.println("box2:");
+        box2.display();
+        box1.setInstance("Hi");
+        System.out.println("box1:");
+        box1.display();
+        System.out.println("box2:");
+        box2.display();
+
+
+        Box<Integer> box3,box4;
+        box3=new Box<>(5);
+        box4=box3;
+        System.out.println("box3:");
+        box3.display();
+        System.out.println("box4:");
+        box4.display();
+        box3.setInstance(6);
+        System.out.println("box3:");
+        box3.display();
+        System.out.println("box4:");
+        box4.display();
+
+        Box<Object> obj1,obj2;
+        obj1= new Box<>("Sail");
+        obj2=obj1;
+        System.out.print("obj1:");obj1.display();
+        System.out.print("obj2:");obj2.display();
+        obj1.setInstance(5);
+        System.out.println("obj1:");
+        obj1.display();
+        System.out.println("obj2:");obj2.display();
+
+        System.out.println(obj2.getInstance());
+
+
+    }
+}
