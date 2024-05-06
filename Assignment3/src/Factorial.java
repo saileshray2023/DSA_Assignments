096dbd31e1d39294976e3e43bb759c5f4f6d0af3
saileshray2023 <saileshray295@gmail.com>
2024-05-06 23:53:02 +0530

    input data type changed to long

--- Assignment3/src/Factorial.java
+++ Assignment3/src/Factorial.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Factorial {
-    public static int fact(int n){
+    public static long fact(long n){
         if (n==0)
             return 1;
         else return n*fact(n-1);
@@ -9,7 +9,7 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number:");
-        int n= sc.nextInt();
-        System.out.printf("Factorial of %d is %d",n,fact(n));
+        long n= sc.nextLong();
+        System.out.printf("Factorial of %d is %d ",n,fact(n));
     }
 }
