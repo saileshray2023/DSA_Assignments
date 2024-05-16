49d90935d580dc1ac2d86ae9dfbafe750a678582
saileshray2023 <saileshray295@gmail.com>
2024-05-16 23:29:30 +0530

    Fibonacci sequence using recursion

---
+++ Assignment3/src/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static long[] fib(int n){
+        if(n<=1){
+            long[] answer={n,0};
+            return answer;
+        }else {
+            long[] temp=fib(n-1);
+            long[] answer={temp[0]+temp[1],temp[0]};
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter n value:");
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(fib(n)[0]);
+    }
+}
