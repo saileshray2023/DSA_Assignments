8e93dd07ba6885cfe8a971791db3ed4c0748a38a
saileshray2023 <saileshray295@gmail.com>
2024-05-20 23:41:20 +0530

    Tower of Hanoi problem

---
+++ Assignment3/src/TowerOfHanoi.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    static void towerOfHanoi(int n, char from_rod, char to_rod, char helper_rod)
+    {
+        if (n == 1)
+        {
+            System.out.println("Take disk 1 from rod " +  from_rod + " to rod " + to_rod);
+            return;
+        }
+        towerOfHanoi(n-1, from_rod, helper_rod, to_rod);
+        System.out.println("Take disk " + n + " from rod " +  from_rod + " to rod " + to_rod);
+        towerOfHanoi(n-1, helper_rod, to_rod, from_rod);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of disks");
+        int n= sc.nextInt();
+        towerOfHanoi(n,'A','C', 'B');
+    }
+}
